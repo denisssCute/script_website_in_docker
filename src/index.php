004057711f3ad06bbdff2bce790a1abeff7e7bdd
denisssCute <denisim1065@gmail.com>
2023-07-04 20:55:14 +0300

    Update and create bash file

--- src/index.php
+++ src/index.php
@@ -1,3 +1,15 @@
 <?php
 echo "Hello World!";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Привет!</h1>
+</body>
+</html>
